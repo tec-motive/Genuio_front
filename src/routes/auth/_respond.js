ceabdea0f93a81acbe5ef564eb00e9ee43367af1
mateo <materokatti@gmail.com>
2021-09-07 13:19:24 +0900

    21-09-07-login session setting finish

--- src/routes/auth/_respond.js
+++ src/routes/auth/_respond.js
@@ -1,4 +1,22 @@
 export function respond(body) {
+	const accountData = [
+		{
+			username: 'doyun',
+			email: 'materokatti@gmail.com',
+			account: 'donfamily',
+			password: 1234,
+			isAdmin: true
+		},
+		{
+			username: 'hyeonjun',
+			email: 'xia@gmail.com',
+			account: 'xia',
+			password: 1234,
+			isAdmin: false
+		}
+	];
+	const { account, password } = body.user;
+
 	if (body.errors) {
 		return { status: 401, body };
 	}
@@ -6,7 +24,12 @@ export function respond(body) {
 	const json = JSON.stringify(body.user);
 	const value = Buffer.from(json).toString('base64');
 
-	//아이디 패스워드를 여기서 확인해야 하지만, 확인 없이 그냥 로그인 잘 됐다고 가정하고 body를 그대로 다시 보내준다.
+	const result = accountData.filter(
+		(user) => user.account === account && user.password === +password
+	);
+
+	body.user = result[0];
+
 	return {
 		headers: {
 			'set-cookie': `jwt=${value}; Path=/; HttpOnly`
